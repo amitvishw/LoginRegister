c3285dca1ec64a6cbd4e21f522ca572861bf1bdd
amitvishw <amit.excellence@gmail.com>
2015-10-10 19:09:16 +0530

    Initial commit

--- app/src/main/java/com/loginregister/UserLocalStore.java
+++ app/src/main/java/com/loginregister/UserLocalStore.java
@@ -1,3 +1,48 @@
 package com.loginregister;
-public class UserLocalStore {
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class UserLocalStore
+{
+    public static final String SP_User="userDetails";
+    SharedPreferences userLocalDatabase;
+    public UserLocalStore(Context context)
+    {
+        userLocalDatabase=context.getSharedPreferences(SP_User,0);
+    }
+    public void storeUserData(User user)
+    {
+        SharedPreferences.Editor spEditor=userLocalDatabase.edit();
+        spEditor.putString("name",user.name);
+        spEditor.putInt("age", user.age);
+        spEditor.putString("username",user.username);
+        spEditor.putString("password",user.password);
+        spEditor.commit();
+    }
+    public User getLoggedInUser()
+    {
+        String name     =userLocalDatabase.getString("name", "");
+        int age         =userLocalDatabase.getInt("age", -1);
+        String username =userLocalDatabase.getString("username", "");
+        String password =userLocalDatabase.getString("password","");
+        User user       =new User(name,age,username,password);
+        return user;
+    }
+    public  void setUserLoggedIn(boolean loggedIn)
+    {
+        SharedPreferences.Editor spEditor=userLocalDatabase.edit();
+        spEditor.putBoolean("loggedIn",loggedIn);
+        spEditor.commit();
+    }
+    public void  clearUserData()
+    {
+        SharedPreferences.Editor spEditor=userLocalDatabase.edit();
+        spEditor.clear();
+        spEditor.commit();
+    }
+    public boolean getUserLoggedIn()
+    {
+        return userLocalDatabase.getBoolean("loggedIn", false);
+    }
 }
